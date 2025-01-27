1a56ec8ab8bc6194f36912ac4e7b3164e4ce2059
Jashanpreet Singh Kaur <sjashan513@gmail.com>
2025-01-27 14:18:56 +0100

    Initial project setup

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    esbuild: {
+        jsxFactory: "h",
+        jsxFragment: "Fragment",
+    },
+    server: {
+        port: 3000,
+    }
+})
---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        globals: true,
+        environment: "jsdom", // for browser-like environment
+        setupFiles: './test/setup.ts', // initialize test environment
+
+    }
+})
